fcbf38b7607548381ef7b8895a1d69cb6a25ff58
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2019-02-26 20:44:20 -0500

    made outlines

---
+++ direct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int changeDirectory(char* args[]){
+  //TODO: Create a function that switches from current directory to specified directory
+}
+
+int makeDirectory(char* args[]){
+  //TODO: Create a function that creates a new file
+}
---
+++ listrem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <termios.h>
+
+void list(args[]){
+  //TODO make a function that prints all of the files within a directory
+}
+
+int remove(args[]){
+
+}
