830068038ffec03454828661584a14827f0f8987
nathanestill <nathan.estill@students.olin.edu>
2019-03-12 20:16:09 -0400

    adding remove mostly

--- listrem.c
+++ listrem.c
@@ -8,6 +8,15 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int is_regular_file(const char *path){
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+}
 
 void printDir(struct dirent* dir, int m){
 	if(m){
@@ -58,7 +67,6 @@ void list(int argc, char* argv[]){
 	            	printf(" ");
 	            	
 	            }
-	            //printf("%d", dir->d_type);
 	        }
         }
         rewinddir(d);
@@ -80,10 +88,63 @@ void list(int argc, char* argv[]){
     }
 }
 
-int removeFile(char* args[]){
-
+int removeFile(int argc, char* argv[]){
+	int r = 0;
+	char ch;
+	while((ch = getopt(argc, argv, "r")) != EOF){
+		switch(ch) {
+			case 'r':
+				r = 1;
+				continue;
+			default:
+				printf("dummy");
+		}
+		argc -= optind;
+		argv += optind;	
+	}
+	printf("%s\n", argv[0]);
+	DIR *d;
+	struct dirent *dir;
+	char* array[2];
+	int status;
+	char directory[strlen(argv[0])+3];
+	strcpy(directory,"./");
+	strcat(directory,argv[0]);
+	if(is_regular_file(argv[0])){
+		status = remove(argv[0]);
+		printf("%d\n", status);
+		if(status){
+			printf("rm: cannot remove '%s': No such file\n",argv[1]);
+		}
+	}
+	else{
+		if(r){
+		    d = opendir(directory);
+		    while ((dir = readdir(d)) != NULL){
+		    	if(dir->d_name[0] != '.'){
+					char nextDir[1+strlen(argv[1])+strlen(dir->d_name)];
+					strcpy(nextDir,argv[0]);
+					strcat(nextDir,"/");
+					strcat(nextDir,dir->d_name);
+					array[0] = "./a.out";
+					array[1] = "-r";
+		    		array[2] = nextDir;
+		    		removeFile(1,array);
+		    	}
+		    }
+		    status = remove(argv[0]);
+			
+		}
+		else{
+			status = remove(argv[0]);
+			if(status){
+			printf("rm: cannot remove '%s': Is a directory\n",argv[0]);
+		}
+		}
+	}
+	return 0;
 }
 
 int main(int argc, char* argv[]){
-	list(argc, argv);
+	removeFile(argc, argv);
 }
