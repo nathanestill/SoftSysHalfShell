c7fdc99717113b9badb4ef1c66fc638c4b6f35ef
nathanestill <nathan.estill@students.olin.edu>
2019-03-14 13:49:09 -0400

    finishing remove I think

--- listrem.c
+++ listrem.c
@@ -95,26 +95,27 @@ int removeFile(int argc, char* argv[]){
 		switch(ch) {
 			case 'r':
 				r = 1;
-				continue;
+				break;
 			default:
-				printf("dummy");
+				printf("it's ok\n");
 		}
 		argc -= optind;
 		argv += optind;	
 	}
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc-1]);
 	DIR *d;
 	struct dirent *dir;
 	char* array[2];
 	int status;
-	char directory[strlen(argv[0])+3];
+	char directory[strlen(argv[argc-1])+3];
 	strcpy(directory,"./");
-	strcat(directory,argv[0]);
-	if(is_regular_file(argv[0])){
-		status = remove(argv[0]);
+	strcat(directory,argv[argc-1]);
+	printf("here\n");
+	if(is_regular_file(argv[argc-1])){
+		status = remove(argv[argc-1]);
 		printf("%d\n", status);
 		if(status){
-			printf("rm: cannot remove '%s': No such file\n",argv[1]);
+			printf("rm: cannot remove '%s': No such file\n",argv[argc-1]);
 		}
 	}
 	else{
@@ -122,23 +123,26 @@ int removeFile(int argc, char* argv[]){
 		    d = opendir(directory);
 		    while ((dir = readdir(d)) != NULL){
 		    	if(dir->d_name[0] != '.'){
-					char nextDir[1+strlen(argv[1])+strlen(dir->d_name)];
-					strcpy(nextDir,argv[0]);
+		    		printf("here\n");
+					char nextDir[1+strlen(argv[argc-1])+strlen(dir->d_name)];
+					strcpy(nextDir,argv[argc-1]);
 					strcat(nextDir,"/");
 					strcat(nextDir,dir->d_name);
+					printf("hernow\n");
 					array[0] = "./a.out";
 					array[1] = "-r";
 		    		array[2] = nextDir;
-		    		removeFile(1,array);
+		    		printf("now\n");
+		    		removeFile(3,array);
 		    	}
 		    }
-		    status = remove(argv[0]);
+		    status = remove(argv[argc-1]);
 			
 		}
 		else{
-			status = remove(argv[0]);
+			status = remove(argv[argc-1]);
 			if(status){
-			printf("rm: cannot remove '%s': Is a directory\n",argv[0]);
+			printf("rm: cannot remove '%s': Is a directory\n",argv[argc-1]);
 		}
 		}
 	}
